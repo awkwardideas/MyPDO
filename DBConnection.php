94a2e740ef9b558a55f2757405ee908b2bc6754b
Chad Haney <chaney@pipelinesuite.com>
2016-02-22 14:23:29 -0700

    Initial Commit

---
+++ DBConnection.php
@@ -0,0 +1,11 @@
+<?php
+namespace AwkwardIdeas\MyPDO;
+
+class DBConnection{
+    public $readOnly;
+    public $readWrite;
+    public function __construct($readOnly, $readWrite){
+        $this->readOnly = $readOnly;
+        $this->readWrite = $readWrite;
+    }
+}
---
+++ MyPDO.php
@@ -0,0 +1,117 @@
+<?php
+namespace AwkwardIdeas\MyPDO;
+
+use AwkwardIdeas\MyPDO\DBConnection as Connection;
+use AwkwardIdeas\MyPDO\SQLParameter as Parameter;
+
+class DB{
+    private static $dbConnection;
+    const DB_ErrorMessage = "Your request was not able to be completed due to a system error has occured";
+
+    public function EstablishConnections($host, $dbname, $rUser, $rPwd, $rwUser, $rwPwd){
+        try {
+            //Establish Read Only Connection
+            if (!isset($mysqlReader) || $mysqlReader == null) {
+                $mysqlReader = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $rUser, $rPwd);
+            }
+            //Establish Read Write Connection
+            if (!isset($mysqlAdmin) || $mysqlAdmin == null) {
+                $mysqlAdmin = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $rwUser, $rwPwd);
+            }
+            self::$dbConnection = new Connection($mysqlReader, $mysqlAdmin);
+            if (isset(self::$dbConnection->readOnly) && isset(self::$dbConnection->readWrite)) {
+                return true;
+            } else {
+                return false;
+            }
+        }catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public function CloseConnections(){
+        self::$dbConnection = null;
+    }
+
+    public function Query($sqlCommand, $sqlParameters = null){
+        try {
+            $readOnly = self::$dbConnection->readOnly;
+            $readOnly->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            if ($sqlParameters != null) {
+                $sqlQuery = $readOnly->prepare($sqlCommand);
+                foreach ($sqlParameters as $sqlParameter) {
+                    $sqlQuery->bindParam($sqlParameter->parameter, $sqlParameter->value, $sqlParameter->dataType);
+                }
+                $sqlQuery->execute();
+                return $sqlQuery->fetchAll();
+            } else {
+                $sqlResponse = $readOnly->query($sqlCommand);
+                return $sqlResponse->fetchAll();
+            }
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public function QueryCount($sqlCommand, $sqlParameters = null){
+        try {
+            $readOnly = self::$dbConnection->readOnly;
+            $readOnly->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            if ($sqlParameters != null) {
+                $sqlQuery = $readOnly->prepare($sqlCommand);
+                foreach ($sqlParameters as $sqlParameter) {
+                    $sqlQuery->bindParam($sqlParameter->parameter, $sqlParameter->value, $sqlParameter->dataType);
+                }
+                $sqlQuery->execute();
+                return sizeof($sqlQuery->fetchAll());
+            } else {
+                $readOnly->query($sqlCommand);
+                $foundRows = $readOnly->query("SELECT FOUND_ROWS()")->fetchColumn();
+                return $foundRows;
+            }
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            return false;
+        }
+    }
+
+    public function Execute($sqlCommand, $sqlParameters){
+        try {
+            $readWrite = self::$dbConnection->readWrite;
+            $readWrite->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sqlQuery = $readWrite->prepare($sqlCommand);
+            foreach ($sqlParameters as $sqlParameter) {
+                $sqlQuery->bindParam($sqlParameter->parameter, $sqlParameter->value, $sqlParameter->dataType);
+            }
+            $sqlQuery->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            return false;
+        }
+    }
+
+    function ExecuteGetIdentity($sqlCommand, $sqlParameters){
+        try {
+            $readWrite = self::$dbConnection->readWrite;
+            $readWrite->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sqlQuery = $readWrite->prepare($sqlCommand);
+            foreach ($sqlParameters as $sqlParameter) {
+                $sqlQuery->bindParam($sqlParameter->parameter, $sqlParameter->value, $sqlParameter->dataType);
+            }
+            $sqlQuery->execute();
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+            return false;
+        }
+
+        $selectIdentityCommand = "SELECT @@IDENTITY AS identity";
+        $sqlQuery = $readWrite->prepare($selectIdentityCommand);
+        $sqlQuery->execute();
+        $identity = $sqlQuery->fetchAll();
+
+        return $identity[0]["identity"];
+    }
+}
